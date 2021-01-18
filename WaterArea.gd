aaf30b7bd0d52f497641f024637c9e7e9e854598
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-01-17 21:44:35 -0800

    Use unit conversion and speed on values from a rgb8.

--- WaterArea.gd
+++ WaterArea.gd
@@ -1,6 +1,7 @@
 extends WaterArea
 
 onready var node = get_node("..")
+onready var river = get_node("../River")
 
 
 func _get_water_heights(positions):
@@ -15,5 +16,7 @@ func _get_water_flow(positions):
 	var ret = []
 	for p in positions:
 		var pos = node.get_water_flow(p)
+		var unit_conv = 10.0
+		pos = pos * river.mat_flow_speed * unit_conv
 		ret.append(pos)
 	return ret
